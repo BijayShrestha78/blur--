557773431aeba7adc2a9461005e7dc62e7c7c481
bijay shrestha <110254041+sthabij@users.noreply.github.com>
2022-08-22 23:16:06 +0545

    outside chnage

---
+++ script.js
@@ -0,0 +1,23 @@
+//js file here
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+
+let load = 0
+
+let int = setInterval(blurring, 40)
+
+function blurring() {
+  load++
+
+  if (load > 89) {
+    clearInterval(int)
+  }
+
+  loadText.innerText = `${load}%`
+  loadText.style.opacity = scale(load, 0, 90, 1, 0)
+  bg.style.filter = `blur(${scale(load, 0, 80, 40, 0)}px)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
